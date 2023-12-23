bfb324649566990341f97b9cb7ba12935ee29e8a
artistic_mammoth <thrl0ad@ya.ru>
2023-12-23 14:10:19 +0300

    Init xcode project

---
+++ BookingApp/BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Михайлов Александр on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
